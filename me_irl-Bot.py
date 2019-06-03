736b7c3fc3f3865cdef7d33b76230e0f3df6222b
iamkolja <45394482+iamkolja@users.noreply.github.com>
2019-06-03 17:18:28 +0200

    Update me_irl-Bot.py
    - added error handler
    - automatically removes messages
    - added status

--- me_irl-Bot.py
+++ me_irl-Bot.py
@@ -1,14 +1,14 @@
 import praw, discord, asyncio, pickle, os, time
+from discord.ext import commands
 from discord.ext.commands import Bot
 
 token   = ''
 bot     = Bot(command_prefix='!')
 isSet   = False
-tempID  = '80ib9u' ## pinned Reddit Submission, which you'll have to hide separately in reddit
+tempID  = '80ib9u'
 channel = ''
 
 ############################### REDDIT #########################################
-
 reddit = praw.Reddit(client_id='',
                      client_secret='',
                      user_agent='',
@@ -18,41 +18,56 @@ reddit = praw.Reddit(client_id='',
 subreddit = reddit.subreddit('me_irl')
 
 ############################### DISCORD ########################################
-
-## SIMPLE COMMAND TO TEST IF THE BOT IS UP AND RUNNING
+## PING COMMAND
 @bot.command(pass_context=True)
 async def ping(ctx):
-    await ctx.message.channel.send(ctx.message.author.mention + ' pong', delete_after=10)
+    await ctx.send(ctx.message.author.mention + ' pong!', delete_after=5)
+    await ctx.message.delete(delay=5)
 
 ## SET COMMAND TO START BROADCAST
 @bot.command(pass_context=True)
+@commands.is_owner()
 async def set(ctx):
-    isAdmin = ctx.message.author.guild_permissions.administrator
-    if isAdmin:
-        global isSet
-        global channel
+    global isSet
+    global channel
+    if not isSet:
         isSet = True
         channel = ctx.message.channel
         pickle.dump(channel.id, open("channel.obj", "wb"))
-        await channel.send("Broadcast started!")
+        await ctx.send("Broadcast started!", delete_after=5)
         print("Broadcast started on " + str(channel) + " #" + str(channel.id))
+        await bot.change_presence(activity=discord.Game(name='me_irl broadcast'))
+        await ctx.message.delete(delay=5)
         await broadcast()
     else:
-        await ctx.message.channel.send(ctx.message.author.mention + " Insufficient Permission!")
-
+        await ctx.send("Already broadcasting!", delete_after=5)
+        await ctx.message.delete(delay=5)
 
 ## UNSET COMMAND TO STOP BROADCAST
 @bot.command(pass_context=True)
+@commands.is_owner()
 async def unset(ctx):
-    isAdmin = ctx.message.author.guild_permissions.administrator
-    if isAdmin:
-        global isSet
+    global isSet
+    if isSet:
         isSet = False
         os.remove("channel.obj")
-        await ctx.message.channel.send("Broadcast stopped!")
+        await ctx.send("Broadcast stopped!", delete_after=5)
         print("Broadcast stopped!")
+        await bot.change_presence(activity=None)
+        await ctx.message.delete(delay=5)
     else:
-        await ctx.message.channel.send(ctx.message.author.mention + " Insufficient Permission!")
+        await ctx.send("Bot is not broadcasting!", delete_after=5)
+        await ctx.message.delete(delay=5)
+
+## ERROR HANDLER
+@bot.event
+async def on_command_error(ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.send("Insufficient permission!", delete_after=5)
+            await ctx.message.delete(delay=5)
+        else:
+            await ctx.send("Error!", delete_after=5)
+            await ctx.message.delete(delay=5)
 
 ## START EVENT
 @bot.event
@@ -60,15 +75,14 @@ async def on_ready():
     global isSet
     global channel
     global tempID
-    print("We are logged in as " + bot.user.name)
+    print("Logged in as " + bot.user.name)
     if os.path.isfile("channel.obj"):
         isSet = True
         channel = bot.get_channel(pickle.load(open("channel.obj", "rb")))
         tempID = pickle.load(open("tempID.obj", "rb"))
-        print("Found old Session!")
+        print("Found old session!")
         print("Broadcast started on " + str(channel) + " #" + str(channel.id))
         await broadcast()
-
 
 ## BROADCAST FUNCTION
 async def broadcast():
@@ -82,7 +96,7 @@ async def broadcast():
                 tempID = submission.id
                 pickle.dump(tempID, open("tempID.obj", "wb"))
             else:
-                print("----------------------")
-        await asyncio.sleep(30)
+                print("----- " + str(time.ctime()) + " -----")
+        await asyncio.sleep(60)
 
 bot.run(token)
